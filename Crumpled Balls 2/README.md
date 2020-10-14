2687dd85eaf9ac3c216273d597b5125822537d9b
Doyel Narang <71204816+DoyelNarang@users.noreply.github.com>
2020-10-14 16:47:18 +0530

    Add files via upload

---
+++ Crumpled Balls 2/README.md
@@ -0,0 +1,4 @@
+# CRUMPLED BALLS 2
+# This project is made to inculcate the habit of throwing the waste in the trash bin in young individuals and help them to keep our country clean. 🗞🗞🧻🧻
+
+NOTE:- Use up-arrow key to throw paper into dustbin.⬆⬆
---
+++ Crumpled Balls 2/paper.js
@@ -0,0 +1,28 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options= {
+          isStatic: false, 
+          restitution:0.3,
+          friction:0,
+          density:1.2
+      }
+
+      this.radius = radius;
+      this.body = Bodies.circle(x, y, this.radius, options);
+
+      this.image = loadImage("paper.png");
+      
+      World.add(world, this.body);
+    }
+    display(){
+
+      fill("purple");
+      var pos1 = this.body.position;
+      push();
+      translate(pos1.x, pos1.y);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.radius, this.radius);
+      pop();
+
+    }
+}
